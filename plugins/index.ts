589fc31df8f8d4e7113f83f119051cda9a306dfb
Elysia <71698422+aiko-chan-ai@users.noreply.github.com>
2026-04-15 15:49:37 +0700

    rename

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,12 +1,12 @@
 import { Plugin } from '@/types/plugin';
-import p_0 from '@plugins/japanese/nocsyosetu';
+import p_0 from '@plugins/japanese/NocSyosetu';
 import p_1 from '@plugins/vietnamese/akaytruyen';
-import p_2 from '@plugins/vietnamese/LNHako';
-import p_3 from '@plugins/vietnamese/tieuthuyetmang';
-import p_4 from '@plugins/vietnamese/truyenfull';
-import p_5 from '@plugins/vietnamese/valvrareteam';
-import p_6 from '@plugins/vietnamese/wanwansekai';
-import p_7 from '@plugins/vietnamese/doctruyenln';
+import p_2 from '@plugins/vietnamese/doctruyenln';
+import p_3 from '@plugins/vietnamese/LNHako';
+import p_4 from '@plugins/vietnamese/tieuthuyetmang';
+import p_5 from '@plugins/vietnamese/truyenfull';
+import p_6 from '@plugins/vietnamese/valvrareteam';
+import p_7 from '@plugins/vietnamese/wanwansekai';
 
 const PLUGINS: Plugin.PluginBase[] = [p_0, p_1, p_2, p_3, p_4, p_5, p_6, p_7];
 export default PLUGINS;
