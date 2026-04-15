13a37e3f409a60059ed6d725456f31cec50161f1
Elysia <71698422+aiko-chan-ai@users.noreply.github.com>
2026-04-15 23:33:27 +0700

    Update BaoMoi.ts

--- plugins/vietnamese/BaoMoi.ts
+++ plugins/vietnamese/BaoMoi.ts
@@ -2,7 +2,7 @@
 
 import { fetchText } from '@libs/fetch';
 import { Plugin } from '@/types/plugin';
-import { Filters } from '@libs/filterInputs';
+import { Filters, FilterTypes } from '@libs/filterInputs';
 import { load as loadCheerio } from 'cheerio';
 import { defaultCover } from '@libs/defaultCover';
 import { NovelStatus } from '@libs/novelStatus';
@@ -12,8 +12,183 @@ class BaoMoiPlugin implements Plugin.PluginBase {
   name = 'Báo Mới';
   icon = 'src/vi/baomoi/icon.png';
   site = 'https://baomoi.com';
-  version = '1.0.1';
-  filters: Filters | undefined = undefined;
+  version = '1.0.2';
+  filters: Filters = {
+    page: {
+      label: 'Tìm theo trang',
+      type: FilterTypes.Picker,
+      options: [
+        { label: 'Tin mới', value: 'tin-moi.epi' },
+        {
+          label: 'Thế giới',
+          value: 'the-gioi.epi',
+        },
+        {
+          label: 'Xã hội',
+          value: 'xa-hoi.epi',
+        },
+        {
+          label: 'Thời sự',
+          value: 'thoi-su.epi',
+        },
+        {
+          label: 'Giao thông',
+          value: 'giao-thong.epi',
+        },
+        {
+          label: 'Môi trường - Khí hậu',
+          value: 'moi-truong-khi-hau.epi',
+        },
+        {
+          label: 'Văn hóa',
+          value: 'van-hoa.epi',
+        },
+        {
+          label: 'Nghệ thuật',
+          value: 'nghe-thuat.epi',
+        },
+        {
+          label: 'Ẩm thực',
+          value: 'am-thuc.epi',
+        },
+        {
+          label: 'Du lịch',
+          value: 'du-lich.epi',
+        },
+        {
+          label: 'Kinh tế',
+          value: 'kinh-te.epi',
+        },
+        {
+          label: 'Lao động - Việc làm',
+          value: 'lao-dong-viec-lam.epi',
+        },
+        {
+          label: 'Tài chính',
+          value: 'tai-chinh.epi',
+        },
+        {
+          label: 'Chứng khoán',
+          value: 'chung-khoan.epi',
+        },
+        {
+          label: 'Kinh doanh',
+          value: 'kinh-doanh.epi',
+        },
+        {
+          label: 'Giáo dục',
+          value: 'giao-duc.epi',
+        },
+        {
+          label: 'Học bổng - Du học',
+          value: 'hoc-bong-du-hoc.epi',
+        },
+        {
+          label: 'Đào tạo - Thi cử',
+          value: 'dao-tao-thi-cu.epi',
+        },
+        {
+          label: 'Thể thao',
+          value: 'the-thao.epi',
+        },
+        /*
+        {
+          label: 'Bóng đá',
+          value: 'listType=3&listId=55',
+        },
+        */
+        {
+          label: 'Bóng đá quốc tế',
+          value: 'bong-da-quoc-te.epi',
+        },
+        {
+          label: 'Bóng đá Việt Nam',
+          value: 'bong-da-viet-nam.epi',
+        },
+        {
+          label: 'Quần vợt',
+          value: 'quan-vot.epi',
+        },
+        {
+          label: 'Giải trí',
+          value: 'giai-tri.epi',
+        },
+        {
+          label: 'Âm nhạc',
+          value: 'am-nhac.epi',
+        },
+        {
+          label: 'Thời trang',
+          value: 'thoi-trang.epi',
+        },
+        {
+          label: 'Điện ảnh - Truyền hình',
+          value: 'dien-anh-truyen-hinh.epi',
+        },
+        {
+          label: 'Pháp luật',
+          value: 'phap-luat.epi',
+        },
+        {
+          label: 'An ninh - Trật tự',
+          value: 'an-ninh-trat-tu.epi',
+        },
+        {
+          label: 'Hình sự - Dân sự',
+          value: 'hinh-su-dan-su.epi',
+        },
+        {
+          label: 'Khoa học - Công nghệ',
+          value: 'khoa-hoc-cong-nghe.epi',
+        },
+        {
+          label: 'CNTT - Viễn thông',
+          value: 'cntt-vien-thong.epi',
+        },
+        {
+          label: 'Thiết bị phần cứng',
+          value: 'thiet-bi-phan-cung.epi',
+        },
+        {
+          label: 'Khoa học',
+          value: 'khoa-hoc.epi',
+        },
+        {
+          label: 'Đời sống',
+          value: 'doi-song.epi',
+        },
+        {
+          label: 'Dinh dưỡng - Làm đẹp',
+          value: 'dinh-duong-lam-dep.epi',
+        },
+        {
+          label: 'Tình yêu - Hôn nhân',
+          value: 'tinh-yeu-hon-nhan.epi',
+        },
+        {
+          label: 'Sức khỏe - Y tế',
+          value: 'suc-khoe-y-te.epi',
+        },
+        {
+          label: 'Xe cộ',
+          value: 'xe-co.epi',
+        },
+        {
+          label: 'Nhà đất',
+          value: 'nha-dat.epi',
+        },
+        {
+          label: 'Quản lý - Quy hoạch',
+          value: 'quan-ly-quy-hoach.epi',
+        },
+        {
+          label: 'Không gian - Kiến trúc',
+          value: 'khong-gian-kien-truc.epi',
+        },
+      ],
+      value: 'tin-moi.epi',
+    },
+  };
   async popularNovels(
     pageNo: number,
     {
@@ -22,27 +197,37 @@ class BaoMoiPlugin implements Plugin.PluginBase {
     }: Plugin.PopularNovelsOptions<typeof this.filters>,
   ): Promise<Plugin.NovelItem[]> {
     const novels: Plugin.NovelItem[] = [];
-    const response = await fetchText(`${this.site}/tin-moi/trang${pageNo}.epi`);
-    const $ = loadCheerio(response);
-    const nextDataScript = $('#__NEXT_DATA__').html();
-    if (nextDataScript) {
-      const nextData = JSON.parse(nextDataScript);
-      const items = nextData.props.pageProps.resp.data.content.items;
-      console.log('Parsed __NEXT_DATA__:', items);
-      items.forEach((item: any) => {
-        if (item.title && item.url) {
-          novels.push({
-            name: item.title,
-            path: item.url,
-            cover: item.thumb || defaultCover,
-          });
-        }
-      });
-    } else {
-      throw new Error(
-        'Failed to find __NEXT_DATA__ script in the popular novels page',
+    if (
+      !filters.page?.value ||
+      (filters.page?.value as string)?.endsWith('.epi')
+    ) {
+      const response = await fetchText(
+        `${this.site}/${(filters.page?.value as string).replace('.epi', '')}/trang${pageNo}.epi`,
       );
+      const $ = loadCheerio(response);
+      const nextDataScript = $('#__NEXT_DATA__').html();
+      if (nextDataScript) {
+        const nextData = JSON.parse(nextDataScript);
+        const items = nextData.props.pageProps.resp.data.content.items;
+        console.log('Parsed __NEXT_DATA__:', items);
+        items.forEach((item: any) => {
+          if (item.title && item.url) {
+            novels.push({
+              name: item.title,
+              path: item.url,
+              cover: item.thumb || defaultCover,
+            });
+          }
+        });
+      } else {
+        throw new Error(
+          'Failed to find __NEXT_DATA__ script in the popular novels page',
+        );
+      }
+    } else {
+      throw new Error('Not implemented for the selected filter option');
     }
+
     return novels;
   }
 
@@ -82,19 +267,21 @@ class BaoMoiPlugin implements Plugin.PluginBase {
           ).toISOString(),
         },
       ];
-      novel.content = `<h1>${data.title}</h1>\n` + data.bodys
-        .map((item: any) => {
-          switch (item.type) {
-            case 'text': {
-              const textClass = item.subType
-                ? ` class="${item.subType}"`
-                : ' class="body-text"';
-              return `<p${textClass}>${item.content}</p>`;
-            }
+      novel.content =
+        `<h1>${data.title}</h1>\n` +
+        data.bodys
+          .map((item: any) => {
+            switch (item.type) {
+              case 'text': {
+                const textClass = item.subType
+                  ? ` class="${item.subType}"`
+                  : ' class="body-text"';
+                return `<p${textClass}>${item.content}</p>`;
+              }
 
-            case 'image': {
-              const loadingAttr = item.lazyLoad ? ' loading="lazy"' : '';
-              return `
+              case 'image': {
+                const loadingAttr = item.lazyLoad ? ' loading="lazy"' : '';
+                return `
           <figure class="article-image">
             <img 
               src="${item.content}" 
@@ -105,10 +292,10 @@ class BaoMoiPlugin implements Plugin.PluginBase {
             />
           </figure>
         `.trim();
-            }
+              }
 
-            case 'video':
-              return `
+              case 'video':
+                return `
           <figure class="article-video">
             <video controls width="${item.width}" height="${item.height}" poster="${item.poster}">
               <source src="${item.content}" type="video/mp4" />
@@ -117,12 +304,12 @@ class BaoMoiPlugin implements Plugin.PluginBase {
           </figure>
         `.trim();
 
-            default:
-              console.warn(`Bỏ qua type không xác định: ${item.type}`);
-              return '';
-          }
-        })
-        .join('\n');
+              default:
+                console.warn(`Bỏ qua type không xác định: ${item.type}`);
+                return '';
+            }
+          })
+          .join('\n');
     } else {
       throw new Error('Failed to find __NEXT_DATA__ script in the novel page');
     }
